73fe06b7e26f000fa2ef8213c4e819ed0519b925
KralLee <410592634@qq.com>
2022-04-11 10:39:51 +0800

    Initial Commit

---
+++ Progress/ProgressApp.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressApp.swift
+//  Progress
+//
+//  Created by LiKai on 2022/4/11.
+//  
+//
+
+import SwiftUI
+
+@main
+struct ProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
